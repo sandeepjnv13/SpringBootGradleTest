c8e368f64bdeedbf44da6880c8d0964cb3012b56
Sandeep <sandeepjnv13@gmail.com>
2019-12-05 03:11:41 +0530

    added login page

--- src/main/java/com/Vortex/Test/Controllers/TestController.java
+++ src/main/java/com/Vortex/Test/Controllers/TestController.java
@@ -1,18 +1,27 @@
 package com.Vortex.Test.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
 public class TestController {
 
+    @RequestMapping("/login.html")
+    public String login() {
+        return "newLoginPanda.html";
+    }
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseBody
     String test(){
         return "<h1>welcome</h1>";
     }
 
     @RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+    @ResponseBody
     public String welcomePage() {
 
 
@@ -22,6 +31,7 @@ public class TestController {
 
 
     @RequestMapping(value = "/admin**", method = RequestMethod.GET)
+    @ResponseBody
     public String adminPage() {
 
         return "welcome admin";
@@ -29,6 +39,7 @@ public class TestController {
     }
 
     @RequestMapping(value = "/dba**", method = RequestMethod.GET)
+    @ResponseBody
     public String dbaPage() {
 
 
--- src/main/java/com/Vortex/Test/SecurityConfig.java
+++ src/main/java/com/Vortex/Test/SecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebSecurity
@@ -23,9 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 //.passwordEncoder(passwordEncoder)
-                .withUser("user").password(passwordEncoder.encode("123456")).roles("USER")
+                .withUser("user").password(passwordEncoder.encode("user")).roles("USER")
                 .and()
-                .withUser("admin").password(passwordEncoder.encode("123456")).roles("USER", "ADMIN");
+                .withUser("admin").password(passwordEncoder.encode("admin")).roles("USER", "ADMIN");
     }
 
     @Bean
@@ -35,31 +41,53 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*http.authorizeRequests()
-                .antMatchers("/login")
-                .permitAll()
-                .antMatchers("/**")
-                .hasAnyRole("ADMIN", "USER")
+        http.authorizeRequests()
+                .antMatchers( "/public/**").permitAll()
+                .anyRequest()//allow all urls
+                .authenticated()//all URLs are allowed by any authenticated user, no role restrictions.
+                .and()
+                .formLogin()//enable form based authentication
+                .loginPage("/login.html")//use a custom login URI
+                .permitAll(true)//login URI can be accessed by anyone
+                .and()
+                .logout()//default logout handling
+                .logoutSuccessUrl("/my-login?logout")//our new logout success url, we are not replacing other defaults.
+                .permitAll();//allow all as it will be accessed when user is not logged in anymore
+       /* http
+                .authorizeRequests()
+                .antMatchers( "/public/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/home")
-                .failureUrl("/login?error=true")
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login?logout=true")
-                .invalidateHttpSession(true)
-                .permitAll()
-                .and()
-                .csrf()
-                .disable();*/
-
-        http
+                .loginPage("/login.html")
+                .failureUrl("/login-error.html")
+                .permitAll();  */
+        /*http
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated()
+                .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .and()
+                .httpBasic(); */
     }
+
+    /*@Bean
+    WebMvcConfigurer myWebMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                ViewControllerRegistration r = registry.addViewController("/my-login");
+                r.setViewName("myLogInPage");
+            }
+        };
+    }*/
+
+    /*@Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }*/
 }
